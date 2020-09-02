abb3700c8a819cb410d1551a00db2fcabc8ca044
Chad Laflamme <cjlaflamme1@plymouth.edu>
2020-09-01 21:35:34 -0400

    figured out express.static finally

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const PORT = process.env.PORT || 5001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
+app.use(express.static('db'));
 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
