008d0e9d1ceaff0e4ec84a4c890e6749fa4e558d
Chad Laflamme <cjlaflamme1@plymouth.edu>
2020-09-01 20:02:27 -0400

    html routes linked

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // API Routes here
-// HTML Routes here
+require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, () => {
     console.log(`App listening on PORT: ${PORT}`);
