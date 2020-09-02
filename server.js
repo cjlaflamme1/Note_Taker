cb8eb432f218e660813d3d34208efa80173db9e8
Chad Laflamme <cjlaflamme1@plymouth.edu>
2020-09-01 20:29:11 -0400

    app.post configured

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// API Routes here
+require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, () => {
