2995b73e772bfaee536d47a77f71b406a47dfea5
Chad Laflamme <cjlaflamme1@plymouth.edu>
2020-09-03 20:43:22 -0400

    added IDs to each note for deletion

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,8 @@
 const path = require('path');
 const db = require('../db/db.json');
+const fs = require('fs');
+
+
 
 module.exports = (app) => {
     app.get("/api/notes", (req, res) => {
@@ -7,7 +10,18 @@ module.exports = (app) => {
     });
 
     app.post("/api/notes", (req, res) => {
-        db.push(req.body);
+        const { title, text} = req.body;
+        let id = 1;
+        if(db.length !== 0) {
+            const lastItem = db[db.length -1];
+            const {id: oldID} = lastItem;
+            id = oldID + 1;
+        }
+        db.push({id, title, text});
+        fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(db), (err) => {
+            if(err) throw err;
+            console.log('The file has been saved.')
+        })
         res.json(db);
     });
 }
